143b3f8d0683ce231868514a8ed767762468f7de
IsaacMellonie <izeymelon@yahoo.com>
2024-02-20 09:51:25 +1000

    add useState for title and bio in UpdateAbout.jsx with handleSave function.

--- src/components/ArtistPortal/UpdateAbout.jsx
+++ src/components/ArtistPortal/UpdateAbout.jsx
@@ -1,16 +1,51 @@
-import react from 'react'
+import react, { useState } from 'react'
 import '../styles/artistportal-styles.css'
 import '../../index.css'
 
 
 const UpdateAbout = () => {
+
+    const [title, setTitle] = useState("") 
+    const [bio, setBio] = useState("")
+
+    const handleSave = async () => {
+        const userData = { title, bio }
+        try {
+            const res = await fetch("/placeholder-api/updateUser", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(userData)
+            })
+
+            if (res.ok) {
+                console.log("Update Successful")
+            } else {
+                console.log("Update Failed")
+            }
+        } catch (error) {
+            console.log("Error: " + error)
+        }
+    }
+
     return(
         <> 
             <div id="about">
                 <h3>About</h3>
-                <textarea name="title" placeholder="Title" id="title"></textarea>
-                <textarea name="bio" placeholder="Bio" id="bio"></textarea>
-                <button id="save-button">Save</button>
+                <textarea 
+                    name="title" 
+                    placeholder="Title" 
+                    id="title"
+                    value={ title }
+                    onClick={(e) => setTitle(e.target.value)}
+                    ></textarea>
+                <textarea 
+                    name="bio" 
+                    placeholder="Bio" 
+                    id="bio"
+                    value={ bio }
+                    onClick={(e) => setBio(e.target.value)}
+                    ></textarea>
+                <button id="save-button" onClick={handleSave}>Save</button>
             </div>
         </>
     )

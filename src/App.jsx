798ca872beba8b204a7c3450867bde9f7447e660
ibehanwillett <imogenbdubs@gmail.com>
2024-02-19 19:32:13 +1000

    Start of tracking items using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import { BrowserRouter, Route, Routes, useParams } from "react-router-dom"
+import { useEffect, useState } from 'react'
 import ShopFront from './components/Shopfront/Shopfront'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Cart from './components/Cart/Cart';
 
-class Item {
-  constructor(name, price, description, image) {
-  this.name = name;
-  this.price = price;
-  this.description = description;
-  this.image = image
-  }
-}
-
-const items = [
- new Item("cool shirt", 28, "a real cool shirt", "https://placekitten.com/200/200"),
- new Item("weird shoes", 30, "very weird shoes", "https://placekitten.com/200/200"), 
- new Item("ugly earrings", 2, "terribly ugly earrings", "https://placekitten.com/200/200")]
-
 
 
 function App() {
+  class Item {
+    constructor(name, price, description, image) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.image = image
+    }
+  }
+
+  const items = [
+  new Item("cool shirt", 28.00, "a real cool shirt", "https://placekitten.com/200/200"),
+  new Item("weird shoes", 30.00, "very weird shoes", "https://placekitten.com/200/200"), 
+  new Item("ugly earrings", 2.00, "terribly ugly earrings", "https://placekitten.com/200/200")]
+
+  const[cartItems, setCartItems] = useState([])
+  useEffect(() => {
+    console.log("loaded")
+    }, [cartItems]
+  )
 
   return (
     <>
@@ -29,9 +35,9 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<ShopFront items={items} />}/>
+                <Route path="/shop" element={<ShopFront items={items} setCartItems= {setCartItems} />}/>
                 <Route path="/about" element={<About />}/>
-                <Route path="/cart" element={<Cart />}/>
+                <Route path="/cart" element={<Cart cartItems={cartItems} />}/>
             </Routes>
         </BrowserRouter>
     </>
--- src/components/Shopfront/Shopfront.jsx
+++ src/components/Shopfront/Shopfront.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/cartItem-styles.css'
 
-const ShopFront = ({ items }) => {
+const ShopFront = ({ items, setCartItems }) => {
 
   
   
@@ -29,7 +29,7 @@ const ShopFront = ({ items }) => {
         <h5>${item.price}</h5>
         <h6>{item.description}</h6>
         <img className="cart-item-image" src={item.image}/>
-        <button>Add to cart</button>
+        <button onClick={setCartItems(cartItems => [...cartItems, item])}>Add to cart</button>
       </div>)
         })
       }

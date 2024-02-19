5c33889f43a604a12d15dadf2106ad4d92f6b86b
ibehanwillett <imogenbdubs@gmail.com>
2024-02-19 18:18:03 +1000

    added iteration through list of objects

--- src/components/Shopfront.jsx
+++ src/components/Shopfront.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 
-const ShopFront = (item) => {
+const ShopFront = () => {
+
+  class Item {
+    constructor(name, price, description, image) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.image
+    }
+  }
+  
+//   const shirt = {
+//     name: "Fun shirt",
+//     price: 28,
+//     description: "Sure is a fun shirt",
+//     image: 
+//     "https://placekitten.com/200/200" 
+// }
+
+const things = [new Item("cool shirt", 28, "a real cool shirt", "https://placekitten.com/200/200"), new Item("weird shoes", 30, "very weird shoes", "https://placekitten.com/200/200")]
+console.log(things[`{index}`])
   return (
     <>
     <h3>ITEMS</h3>
-    <div>shopfront</div>
+    <div>
+      {
+      things.map((index) => {
+        <>
+        
+      <h4>`{things[{index}].name}</h4>
+      <h5>$`{things[{index}].price}</h5>
+      <h6>`{things[{index}].description}</h6>
+      <img src={things[{index}].image}/>
+        </> 
+        })
+      }
+    </div>
     </>
   )
 }

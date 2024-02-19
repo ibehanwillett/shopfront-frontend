64b5f3f620ced6b273ec3a312760a5c68ec84d4a
ibehanwillett <imogenbdubs@gmail.com>
2024-02-19 11:35:04 +1000

    Intial commit on new branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ import Home from './components/Home'
 import About from './components/About'
 import Cart from './components/Cart/Cart'; 
 
+class Item {
+  constructor(name, price, description, image) {
+  this.name = name;
+  this.price = price;
+  this.description = description;
+  this.image = image;
+  }
+}
+
+shirt = new Item("Fun shirt", 28, "Sure is a fun shirt", "https://placekitten.com/200/300")
+
+
 
 function App() {
 
@@ -14,7 +26,7 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<ShopFront />}/>
+                <Route path="/shop" element={<ShopFront item={shirt} />}/>
                 <Route path="/about" element={<About />}/>
                 <Route path="/cart" element={<Cart />}/>
             </Routes>
--- src/components/Shopfront.jsx
+++ src/components/Shopfront.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ShopFront = () => {
+const ShopFront = (item) => {
   return (
     <>
     <h3>ITEMS</h3>

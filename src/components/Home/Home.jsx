06ca13cc9c8c7b74dc9498f622933ca58fa571f9
IsaacMellonie <izeymelon@yahoo.com>
2024-02-28 19:10:07 +1000

    map items for featured on homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,19 +4,19 @@ import { ItemsContext } from '../../app-context/ItemsContext'
 // import '../styles/shop-styles.css'
 import '../styles/home-styles.css'
 import picture from '../../assets/watercolour_square.png'
+import ShopfrontItem from '../Shopfront/ShopfrontItem'
 
 
 const Home = () => {
 
   const { items } = useContext(ItemsContext)
+  const featuredItems = items.filter(item => item.featured)
 
   return (
     <>
     <div className="container">
       <div className="section">
-        <div id="image-container">
-          <img id="home-image" src={picture} alt="Nicole Nightmare Home Image" />
-        </div>
+        <img id="home-image" src={picture} alt="Nicole Nightmare Home Image" />
         <div id="text-container">
           <h2 id="welcome">WELCOME</h2>
           <p id="body-text">
@@ -27,11 +27,25 @@ const Home = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </div>
-        
       </div>
+      <div className="featured-items">
+          <h1 id="items-heading">Featured</h1>
+          <div className="items-grid">
+            {featuredItems.map((item, index) => (
+              <ShopfrontItem
+                key={index}
+                item={item}
+                link={`/item/${item._id}`}
+                name={item.name}
+                price={item.price}
+                description={item.description}
+                image={item.image}
+                disableLink={false}
+              />
+            ))}
+          </div>
+        </div>
     </div>
-      
-    
     </>
   )
 }

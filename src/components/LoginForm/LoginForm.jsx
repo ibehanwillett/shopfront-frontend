7d89c8b807f5ecc58828e0e1587df3b024a77c35
ibehanwillett <imogenbdubs@gmail.com>
2024-03-01 00:05:52 +1000

    commented EditForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, Link } from "react-router-dom"
-import TextAreaField from "../ArtistPortal/ItemComponents/TextAreaField.jsx"
 import { useUserContext } from '../../app-context/UserContext.jsx'
 import "../styles/login-style.css"
 
